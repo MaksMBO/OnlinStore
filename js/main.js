3e94d08f2b5b71375eebcbc2ac28e501e87ceea5
MaksMBO <maksbredyk@gmail.com>
2022-07-17 18:11:03 +0300

    new catalog

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn') & !event.target.matches('.dropbtn__img') & !event.target.matches('.dropbtn__span')) {
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn') & !event.target.matches('.dropbtn__img') & !event.target.matches('.dropbtn__span')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+//   var dropdowns = document.getElementsByClassName("dropdown-content");
+//   var i;
+//   for (i = 0; i < dropdowns.length; i++) {
+//     var openDropdown = dropdowns[i];
+//     if (openDropdown.classList.contains('show')) {
+//       openDropdown.classList.remove('show');
+//     }
+//   }
+// }
+// }
+
+
+let buttonCatalog = document.querySelector("button.dropbtn");
+let menu = document.querySelector("div.myDropdown")
+
+buttonCatalog.addEventListener("mouseenter", function () {
+  document.getElementById("myDropdown").classList.toggle("show");
+});
