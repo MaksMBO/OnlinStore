0421c3391fa0c09a5345c8a6708b95f719e70d9c
MaksMBO <maksbredyk@gmail.com>
2022-07-19 18:04:15 +0300

    added register and mobile

--- js/main.js
+++ js/main.js
@@ -4,8 +4,21 @@ function showLeftMenu() {
 }
 
 window.onclick = function (event) {
-  if (!event.target.matches('.dropdown-content__mobile') & !event.target.matches('.dropbtn__mobile') & !event.target.matches('.dropbtn__img')) {
+  if (!event.target.matches('.dropdown-content__mobile') & !event.target.matches('.dropbtn__mobile') & !event.target.matches('.dropbtn__img')
+    & !event.target.matches('.not__close')) {
     document.getElementById("myDropdown__mobile").style.display = "none";
+    document.body.style.overflow = 'auto';
+  }
+}
+
+function showRegister() {
+  document.getElementById("register").style.display = "flex";
+  document.body.style.overflow = 'hidden';
+}
+
+window.onclick = function (event) {
+  if (event.target.matches('.register') & !event.target.matches('.header__user__button') & !event.target.matches('.header__user__text')) {
+    document.getElementById("register").style.display = "none";
     document.body.style.overflow = 'auto';
   }
 }
