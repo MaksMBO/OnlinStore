63b774866955283f0676c4ed09fcc4aad1563858
MaksMBO <maksbredyk@gmail.com>
2022-07-21 19:43:00 +0300

    finaly

--- js/slider__info.js
+++ js/slider__info.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
 
     $('.slider_nav').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        // asNavFor: '.slider-products',
-        dots: true,
-        centerMode: true,
         focusOnSelect: true,
         vertical: true,
+        arrows: false,
+        adaptiveHeight: true,
+        slidesToShow: 7,
+        asNavFor: ".slider_nav_big",
     });
+
+    $('.slider_nav_big').slick({
+        arrows: false,
+        fade: true,
+        asNavFor: ".slider_nav",
+        draggable: false,
+    })
 });
--- js/slider__main.js
+++ js/slider__main.js
@@ -8,4 +8,6 @@ $(document).ready(function () {
         draggable: false,
 
     });
+
+    
 });
