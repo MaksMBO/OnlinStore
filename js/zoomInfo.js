72ca6a6013d27270fb45d25692da920fb5504dbf
MaksMBO <maksbredyk@gmail.com>
2022-07-27 17:33:12 +0300

    mouseleave mouseenter

--- js/zoomInfo.js
+++ js/zoomInfo.js
@@ -1,7 +1,11 @@
-
-function zoom(receivedIMG, placeIMG) {
+function zoom(receivedIMG, placeIMG, placeIMGId) {
     let allLinks = document.querySelector(receivedIMG);
     let linkIMG = allLinks.getAttribute('src');
-
+   
     $(placeIMG).html(`<div class='test position-absolute'><img src='${linkIMG}'class= 'full-image'></div >`);
+     document.getElementById(`${placeIMGId}`).style.display = "block";
+}
+
+function zoomLeave(placeIMG) {
+    document.getElementById(`${placeIMG}`).style.display = "none";
 }
