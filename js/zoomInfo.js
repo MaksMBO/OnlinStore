46012e19ca60e423c4456156366ad0babe14018a
MaksMBO <maksbredyk@gmail.com>
2022-08-02 15:08:24 +0300

    improved zoom

--- js/zoomInfo.js
+++ js/zoomInfo.js
@@ -26,9 +26,9 @@ $('.slider_nav__item_big').on('mousemove', function () {
     let img = $('.slick-current .full-image').attr('src');
     let imgBlock = $(this).find('img');
     let imgWidth = imgBlock.width();
-    let imgHeight = imgBlock.height();
     let overlay = $('.containerForZoom');
     let cursor = $('.zoom__cursor');
+    let maxWidth = $('.slider_nav_big').width();
     cursor.css('width', overlay.width() / zoom + 'px');
     cursor.css('height', overlay.height() / zoom + 'px');
     let cursorWidth = cursor.outerWidth();
@@ -36,22 +36,15 @@ $('.slider_nav__item_big').on('mousemove', function () {
     let posX = globalX - $(this).offset().left - cursorWidth / 2;
     let posY = globalY - $(this).offset().top - cursorHeight / 2;
 
-    let maxWidth = $('.slider_nav_big').width();
-
-
-    if (posY < 0) {
-        posY = 0;
-    }
-
     if (posX < maxWidth / 2 - imgWidth / 2) {
         posX = maxWidth / 2 - imgWidth / 2;
     }
-
-
+    if (posY < 0) {
+        posY = 0;
+    }
     if (posX > maxWidth / 2 + imgWidth / 2 - cursorWidth) {
         posX = maxWidth / 2 + imgWidth / 2 - cursorWidth;
     }
-
     if (posY > ($(this).height() - cursorHeight)) {
         posY = $(this).height() - cursorHeight;
     }
@@ -64,13 +57,10 @@ $('.slider_nav__item_big').on('mousemove', function () {
     posX *= zoom;
     posY *= zoom;
 
-    
-
     overlay.css('background-image', `url(${img})`);
     overlay.css('background-size', (imgWidth * zoom) + 'px');
     overlay.css('background-position', `${-posX}px ${-posY}px`);
     overlay.show();
-
 });
 
 $('.slider_nav_big').on('mouseleave', function () {
