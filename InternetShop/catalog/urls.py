b8975c2a581e9c5b27c6a7651d33d3946bd5cee6
MaksMBO <maksbredyk@gmail.com>
2023-02-04 23:42:29 +0200

    add page of product

--- InternetShop/catalog/urls.py
+++ InternetShop/catalog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
  path('', views.Catalog.as_view(), name="catalog"),
+ path('name/', views.AboutProduct.as_view(), name='aboutProduct'),
 ]
--- InternetShop/catalog/views.py
+++ InternetShop/catalog/views.py
@@ -19,6 +19,21 @@ class Catalog(Error, ListView):
         self.temp_error = self.errors.copy()
         self.errors.clear()
         
-        return render(request, 'catalog\catalog.html', context={
+        return render(request, 'catalog/catalog.html', context={
             'register_form': user_register, 'email_login_form': user_email_login,
             'phone_login_form': user_phone_login, "errors": self.temp_error})
+        
+class AboutProduct(Error, ListView):
+    def get(self, request):
+        user_register = UserRegistrationForm()
+        user_email_login = UserEmailLogin()
+        user_phone_login = UserPhoneLogin()
+        
+        self.temp_error.clear()
+        self.temp_error = self.errors.copy()
+        self.errors.clear()
+        
+        return render(request, 'catalog/aboutProduct.html', context={
+            'register_form': user_register, 'email_login_form': user_email_login,
+            'phone_login_form': user_phone_login, "errors": self.temp_error})
+    
