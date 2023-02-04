cba57bd0f5ae831173a987b758e45392f5130f0a
MaksMBO <maksbredyk@gmail.com>
2023-02-04 23:59:17 +0200

    add comparison

--- InternetShop/catalog/urls.py
+++ InternetShop/catalog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
  path('', views.Catalog.as_view(), name="catalog"),
  path('name/', views.AboutProduct.as_view(), name='aboutProduct'),
+ path('comparison/', views.ProductComparison.as_view(), name='productComparison')
 ]
--- InternetShop/catalog/views.py
+++ InternetShop/catalog/views.py
@@ -36,4 +36,19 @@ class AboutProduct(Error, ListView):
         return render(request, 'catalog/aboutProduct.html', context={
             'register_form': user_register, 'email_login_form': user_email_login,
             'phone_login_form': user_phone_login, "errors": self.temp_error})
+        
+        
+class ProductComparison(Error, ListView):
+    def get(self, request):
+        user_register = UserRegistrationForm()
+        user_email_login = UserEmailLogin()
+        user_phone_login = UserPhoneLogin()
+        
+        self.temp_error.clear()
+        self.temp_error = self.errors.copy()
+        self.errors.clear()
+        
+        return render(request, 'catalog/productComparison.html', context={
+            'register_form': user_register, 'email_login_form': user_email_login,
+            'phone_login_form': user_phone_login, "errors": self.temp_error})
     
