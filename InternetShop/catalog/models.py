5810003fee99df3d8f45d82d828bda1369768e57
MaksMBO <maksbredyk@gmail.com>
2023-02-05 19:27:36 +0200

    added models

--- InternetShop/catalog/models.py
+++ InternetShop/catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class ProductsImg(models.Model):
+    img = models.FileField(upload_to='uploads/')
+
+
+class ProductsType(models.Model):
+    type = models.CharField(max_length=200, default=None)
+
+
+class ProductsBrand(models.Model):
+    brand = models.CharField(max_length=200, default=None)
+
+
+class Products(models.Model):
+    title = models.CharField(max_length=200, default=None)
+    price =  models.DecimalField(max_digits=10, decimal_places=2)
+    img = models.ForeignKey(ProductsImg, on_delete=models.CASCADE)
+    type = models.ForeignKey(ProductsType, on_delete=models.CASCADE)
+    brand = models.ForeignKey(ProductsBrand, on_delete=models.CASCADE)
--- InternetShop/users_page/models.py
+++ InternetShop/users_page/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.urls import reverse
 from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Basket(models.Model):
+    basket = models.ManyToManyField('catalog.Products')
 
 
 class User(AbstractUser):
     """Class for describing a model with a user"""
 
     phone = PhoneNumberField(null=False, blank=False, unique=True)
+    basket = models.ForeignKey(Basket, on_delete=models.CASCADE, blank=True, null=True)
+    like = models.ManyToManyField('catalog.Products', related_name='like', blank=True, null=True)
+    comparison = models.ManyToManyField('catalog.Products', related_name='comparison', blank=True, null=True)
 
     class Meta:
-        db_table = "user"
+        db_table = 'user'
 
     def __str__(self):
-        return f"{self.username}"
+        return f'{self.username}'
