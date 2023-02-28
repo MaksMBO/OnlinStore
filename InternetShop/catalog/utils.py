1010a0ca834677ce30ee2ebcf26c0aa8dc22e36c
Alekssin1 <xomkaxomiakovuch@gmail.com>
2023-02-28 16:10:45 +0200

    Fix for unregistered users

--- InternetShop/catalog/utils.py
+++ InternetShop/catalog/utils.py
@@ -12,31 +12,33 @@ class Notifications:
     temp_error = []
     success = []
     temp_success = []
-    
+
     def add_to_temp(self, list_name, temp_name):
         temp_name.clear()
         temp_name = list_name.copy()
         list_name.clear()
         return temp_name
-    
-    
+
+
 class CatalogMixin(Notifications):
-    
+
     def renderPage(self):
         user_register = UserRegistrationForm()
         user_email_login = UserEmailLogin()
         user_phone_login = UserPhoneLogin()
-        
+
         self.temp_error = self.add_to_temp(self.errors, self.temp_error)
         self.temp_success = self.add_to_temp(self.success, self.temp_success)
-        
+        bin = ''
+        if self.request.user == "AnonymousUser":
+            bin = self.request.user.basket
         context = {
-            'register_form': user_register, 
+            'register_form': user_register,
             'email_login_form': user_email_login,
-            'phone_login_form': user_phone_login, 
+            'phone_login_form': user_phone_login,
             'errors': self.temp_error,
-            'success':self.temp_success,
-            'bin':self.request.user.basket,
+            'success': self.temp_success,
+            'bin': bin,
         }
-        
+
         return context
