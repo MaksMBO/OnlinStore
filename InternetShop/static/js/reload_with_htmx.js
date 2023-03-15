7b68e8e9ea961d533c4b886c958bc97827e74d1b
MaksMBO <maksbredyk@gmail.com>
2023-03-15 21:54:33 +0200

    save htmx content

---
+++ InternetShop/static/js/reload_with_htmx.js
@@ -0,0 +1,17 @@
+function saveState() {
+    var state = {
+      html: document.body.innerHTML,
+    };
+    history.pushState(state, '', window.location);
+  }
+  
+  htmx.on('htmx:afterOnLoad', function(event) {
+    saveState();
+  });
+  
+  window.addEventListener('popstate', function(event) {
+    if (event.state && event.state.html) {
+      document.body.innerHTML = event.state.html;
+    }
+  });
+  
